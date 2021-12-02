79e57c5cc2669612d46476686b32419474f74f88
efloresm99 <efloresm99@gmail.com>
2021-12-02 15:25:55 -0600

    Crear archivo gitignore para node_modules, definir la estructura del proyecto, instalar las dependencias a utilizar y configurarlas

---
+++ src/app.js
@@ -0,0 +1,29 @@
+//Inicializar dependencias requeridas
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+//Configurar puerto a utilizar por la aplicación
+const PORT = process.env.PORT || 3000;
+
+//Definir instancia de express
+const app = express();
+
+//Definir uso de bodyparser
+app.use(bodyParser.json());
+
+//Configurar ejs como motor de plantillas
+app.set('view engine', 'ejs');
+
+//Definir el directorio donde estarán las vistas
+app.set('views', path.join(__dirname, 'views'));
+
+//Configurar la extensión de archivos de modo que sea .html para evitar confusiones
+app.engine('html', require('ejs').renderFile);
+
+//Importar rutas
+
+
+
+//Inicializar aplicación
+app.listen(PORT, () => console.log("Servidor corriendo en puerto " + PORT));
