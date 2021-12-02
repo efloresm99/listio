93ac597c4ed360ee9d7f033bb37cff4dfb5ae663
efloresm99 <efloresm99@gmail.com>
2021-12-02 15:52:09 -0600

    Creado el endpoint de la API que busca un usuario por username

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,6 +23,34 @@ conexion.connect(error => {
 //Sin embargo, por ahora, la lógica de la app estará aquí.
 
 
+//Primero estarán los endpoints de la API
+
+
+//                      Usuarios (o simulación de usuarios)
+
+//Verificar si el usuario existe con el nombre de usuario
+router.get('/api/users/:username', (req, res) => {
+    const {username} = req.params;
+    const queryUser = `SELECT iduser FROM users WHERE username = '${username}'`;
+    conexion.query(queryUser, (error, result) => {
+        let response = {}; //Este objeto será el que devolverán todos los endpoints de las APIs
+        if (error) throw error;
+
+        if (result.length > 0){
+            response.responseCode = 1;
+            response.message = "Usuario encontrado";
+            response.items = result;
+        } else{
+            response.responseCode = 0;
+            response.message = "Usuario no encontrado";
+            response.items = {}
+        }
+
+        //Devolver datos
+        res.json(response);
+    })
+});
+
 
 
 module.exports = router;
